3ffbdc073e9a98cf8489aee3aeb87e68ae57a48e
Hasan <martinhbramwell@gmail.com>
2012-02-22 14:53:40 -0500

    Job backup is now working properly

--- projects/backupJobs.sh
+++ projects/backupJobs.sh
@@ -3,17 +3,53 @@
 #
 export PROJECT=$1
 export MANIFEST=jobList.txt
+export AUTH_TOKEN=VrdKke6c84sEpe4k5dk82p2p9rcO81OC
+export RUNDECK_HOST="http://localhost:4440"
+export JOBLISTING_ACTION="api/1/jobs"
+export JOBEXPORT_ACTION="api/1/jobs/export"
+export AUTH_PARM="authtoken=${AUTH_TOKEN}"
+export PROJECT_PARM="project=${PROJECT}"
+export FORMAT_PARM="format=yaml"
+export PATH_PREFIX="net.fleetingclouds."
 
 if [  -d  ${PROJECT}  ]; then 
 
-	if [  -f  ./${PROJECT}/${MANIFEST}  ]; then 
-		echo Backing up jobs of project : ${PROJECT}
-	else 
-		echo No project jobs list : ./${PROJECT}/${MANIFEST}
-	fi
+	echo Generating draft jobs manifest to ./${PROJECT}/${MANIFEST}
+	wget --quiet "${RUNDECK_HOST}/${JOBLISTING_ACTION}?${AUTH_PARM}&${PROJECT_PARM}" \
+	-O - | xpath -q -e "result/jobs"  -e "job[*]/@id" \
+	 | sed -e "s| id=\"||g" \
+	 | sed -e "s|\"||g" \
+	> ./${PROJECT}/${MANIFEST}
+#
+	echo Now process it
+#
+	mapfile -t JOB_LIST < "./${PROJECT}/${MANIFEST}"
+
+	DEFAULT_LOCATION=`pwd`
+#	echo DEFAULT_LOCATION = ${DEFAULT_LOCATION}
+
+	FULL_URL="${RUNDECK_HOST}/${JOBEXPORT_ACTION}?${AUTH_PARM}&${PROJECT_PARM}&${FORMAT_PARM}&idlist="
+	for JOB in "${JOB_LIST[@]}"
+	do
+		FILE_PATH=`echo $JOB | sed s/$PATH_PREFIX// | sed 's/\(.*\)\..*/\1/' | sed -e 's|\.|/|g' `
+#		echo Path : ${DEFAULT_LOCATION}/${PROJECT}/jobs/${FILE_PATH}
+#		echo File : ${JOB}.yaml
+
+		LOCAL_PATH=${PROJECT}/jobs/${FILE_PATH}
+		FULL_PATH=${DEFAULT_LOCATION}/${LOCAL_PATH}
+		mkdir -p ${FULL_PATH}
+		wget --quiet ${FULL_URL}${JOB} -O ${FULL_PATH}/${JOB}.yaml
+		echo Wrote : ./${LOCAL_PATH}/${JOB}.yaml
+
+	done
+
 else
+
 	echo No such project ${PROJECT}
 fi
+
+echo Done.
+echo . . . .  . . . .  . . . .  . . . . 
 
 
 exit
